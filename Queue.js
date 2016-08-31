0eaeac77b3fdccfe338158ceaacba2fc1181aba4
Ashley Smith <ashleymarie4989@gmail.com>
2016-08-31 15:19:14 -0500

    add stack and queue files

---
+++ Queue.js
@@ -0,0 +1,16 @@
+var Queue = function() {
+
+};
+
+var queueMethods = {};
+
+
+queueMethods.enqueue = function(value) {
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+};
---
+++ Stack.js
@@ -0,0 +1,17 @@
+var Stack = function() {
+
+};
+
+var stackMethods = {};
+
+stackMethods.push = function(value) {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  
+};
